1f2db9f1d3e4d632d1deff06a2bff824b6ddefe6
Scott Currie <vitriolik@gmail.com>
2018-10-02 09:47:51 -0700

    Added Game and Team object constructors

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
 // popup modal
 console.log('getting here');
-    var login = document.getElementById("popup")
-        
-    var myFunc = function(type){
-        console.log('open');
-        login.classList.add("isvisable");
-    }
-    
-    var closePop = function(){
-        if(event.currentTarget === event.target){
-            
-            login.classList.remove("isvisable")
-        }
-    
-    }
+var login = document.getElementById('popup');
 
+var myFunc = function(type){
+  console.log('open');
+  login.classList.add('isvisable');
+};
 
+var closePop = function(){
+  if(event.currentTarget === event.target){
 
-// game constructor function 
+    login.classList.remove('isvisable');
+  }
 
+};
+// Global variables
+var maxRounds = 3;
+Game.teams = [];
 
-
+// game constructor function
+function Game() {
+  this.maxRounds = maxRounds;
+  this.teams = [];
+  this.currentRound = 1;
+}
 
 
 //team constructor function
-
+function Team(teamName) {
+  this.teamName = teamName;
+  this.score = 0;
+  this.gamesPlayed = 0;
+  Game.teams.push(this);
+}
 
 
 
@@ -48,7 +56,7 @@ console.log('getting here');
 
 
 
-// turn start here 
+// turn start here
 
 
 
