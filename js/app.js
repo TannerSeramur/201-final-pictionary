dbf30e4ca03484259413fcf12669e8b36261b459
Tanner Seramur <seramurt@gmail.com>
2018-10-02 09:35:14 -0700

    working on modal

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// popup modal
+console.log('getting here');
+    var login = document.getElementById("popup")
+        
+    var myFunc = function(type){
+        console.log('open');
+        login.classList.add("isvisable");
+    }
+    
+    var closePop = function(){
+        if(event.currentTarget === event.target){
+            
+            login.classList.remove("isvisable")
+        }
+    
+    }
+
+
+
+// game constructor function 
+
+
+
+
+
+//team constructor function
+
+
+
+
+
+// modals for index page here
+
+
+
+
+// add new game
+
+
+
+// add new team
+
+
+
+
+// remove team
+
+
+
+
+// turn start here 
+
+
+
+
+// role switch here
