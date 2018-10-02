610442efefd97fa16525c6045a91e1b12b7fe8f3
Scott Currie <vitriolik@gmail.com>
2018-10-02 10:48:05 -0700

    Added skeleton of function declarations

--- js/app.js
+++ js/app.js
@@ -43,18 +43,82 @@ function removeTeam(teamName) {
   }
 }
 
-
-function startGame() {
-  // This gets kicked off when the play button on index.html gets clicked
-
+// when instructions button on index.html gets clicked
+function showInstructions() {
+  // TODO: implement show instructions modal
 }
 
+// called in playGame
+function chooseTeams() {
+  // Display the modal for choosing teams
+  showTeamSelect();
+}
+
+// called in chooseTeams
+function showTeamSelect() {
+  // TODO: Display the modal for team selection
+  // Render the teams list into the team select modal
+  renderTeams();
+}
+
+// called in showTeamSelect
+function renderTeams() {
+  // TODO: make Games.teams show up in the team select modal
+}
+
+// When the play button on index.html gets clicked
+function playGame() {
+  // Do all the choose team things before we can start
+  chooseTeams();
+  // loop over the number of rounds in our game
+  for (let round = 1; round <= game.maxRounds; round++) {
+    // Do start round things here
+    startRound();
+    // each round, loop over the number of teams in our game
+    for (let currentTeam = 0; currentTeam < Game.teams.length; currentTeam++) {
+      // Do start turn things here
+      doTurn(Game.teams[currentTeam]);
+    }
+  } 
+  // Game is done. Show results.
+  showEndOfGameResults();
+}
+
+function startRound() {
+
+}
 
 // turn start here
-function startTurn() {
-
+function doTurn(currentTeam) {
+  // Accepts a team object as parameter
+  promptDrawer();
+  startDraw();
+  promptGuesser();
+  startGuessing();
+  showTurnResults();
 }
 
+function promptDrawer() {
+  // TODO: do the prompt
+}
 
+function startDraw() {
+  // TODO: run drawing code
+}
 
+function promptGuesser() {
+  // TODO: show the prompt
+}
+
+function startGuessing() {
+  // TODO: implement guessing code
+}
+
+function showTurnResults() {
+  // TODO: display end of turn results
+}
+
+function showEndOfGameResults() {
+  // TODO: display end of game results
+}
 // role switch here
